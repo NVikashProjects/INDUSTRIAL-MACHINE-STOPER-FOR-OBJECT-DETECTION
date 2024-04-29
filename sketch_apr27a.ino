c6dac6465b15263f7b47700017af52d7a77056f2
NVikashProjects <165534119+NVikashProjects@users.noreply.github.com>
2024-04-29 21:53:32 +0530

    updated file

--- sketch_apr27a.ino
+++ sketch_apr27a.ino
@@ -2,8 +2,10 @@
 #include "Ultrasonic.h"
 #include "MotorDrive.h"
 #include <LiquidCrystal_I2C.h>
+#include <SoftwareSerial.h>
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+SoftwareSerial gsm(12, 13);
 
 ServoMotor servo;
 Ultrasonic ultrasonic;
@@ -12,11 +14,12 @@ MotorDrive motordrive;
 int servomotor = 11, i = 0, j = 1;
 int triger = 2, echo = 3;
 int enable1 = 5, enable2 = 6, MF1 = 7, MB1 = 8, MF2 = 9, MB2 = 10;
-int ir = 12;
+int ir = 4;
 int distance, previous;
 
 void setup() {
   Serial.begin(9600);
+  gsm.begin(9600);
   ultrasonic.UltrasonicSetup(triger, echo);
   servo.ServoSetup(servomotor);
   pinMode(ir, INPUT);
@@ -24,10 +27,11 @@ void setup() {
   lcd.init();
   lcd.clear();
   lcd.backlight();
+  gsm.println("AT");
+  delay(5000);
 }
 
 void loop() {
-
 
   servo.ServoAngle(i);
   delay(10);
@@ -35,20 +39,43 @@ void loop() {
   if (digitalRead(ir) == 0 || distance < 20) {
     motordrive.Motor(0, "OFF");
     Serial.println("Motor Stoped ...!");
+
+
     if (digitalRead(ir) == 0) {
       lcd.clear();
       lcd.setCursor(0, 1);
       lcd.print("Fire detected");
       lcd.setCursor(0, 0);
       lcd.print("Motor Stoped");
+
+      gsm.println("AT");
+      delay(1000);
+      gsm.println("AT+CMGF=1");
+      delay(500);
+      gsm.println("AT+CMGS=\"+917708714210\"");
+      delay(500);
+      gsm.print("Fire detected");
+      delay(500);
+      gsm.write(26);
     } else {
       lcd.clear();
       lcd.setCursor(0, 1);
       lcd.print("Object detected");
       lcd.setCursor(0, 0);
       lcd.print("Motor Stoped");
+      
+      gsm.println("AT");
+      delay(1000);
+      gsm.println("AT+CMGF=1");
+      delay(500);
+      gsm.println("AT+CMGS=\"+917708714210\"");
+      delay(500);
+      gsm.print("Object detected");
+      delay(500);
+      gsm.write(26);
     }
     servo.ServoAngle(previous);
+    lcd.clear();
   } else {
     if (i <= 180 && j == 1) {
       i = i + 1;
@@ -63,8 +90,7 @@ void loop() {
     }
     motordrive.Motor(200, "Front");
     Serial.println("Motor Running ...");
-    previous = i;
-    lcd.clear();
+    previous = i;    
     lcd.setCursor(0, 0);
     lcd.print("Motor Running");
   }
